2e396b5980baacd1fe4e7e53be7fcc071f2a017d
SkyG0D <silvanosilvino@hotmail.com>
2020-05-10 07:41:24 -0300

    Estudos dia 10/05/2020

--- src/br/com/sky/javacore/Texpressoesregulares/TokenTest.java
+++ src/br/com/sky/javacore/Texpressoesregulares/TokenTest.java
@@ -3,7 +3,7 @@ package br.com.sky.javacore.Texpressoesregulares;
 public class TokenTest {
     public static void main(String[] args) {
         String str = "Silvano, Vitor, Victor, Helbnny, Mikael, Marlos, Christian";
-        String[] tokens = str.split("");
+        String[] tokens = str.split(",");
         for (String array : tokens ) {
             System.out.println(array.trim());
         }
